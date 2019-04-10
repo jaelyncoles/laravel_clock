fc81e3b75e27d526b946c471e309267bb8d07842
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-04-10 14:28:40 -0400

    working clock start countdown

---
+++ clock/resources/js/components/clock-component.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+          
+          <div class="content">
+            <div id="txt" class="display-1">{{h}}:{{m}}:{{s}}</div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: "clock-component",
+  data() {
+    return {
+      h: 0,
+      m: 0,
+	  s: 0,
+	  interval: null
+    };
+  },
+  methods: {
+    startTime: function() {
+	  
+	  var self = this;
+	  console.log(this);
+	  // var t = setInterval(this.startTime(), 1000);  // this is relative to setInterval
+      this.interval = setInterval(() => {
+		  console.log(this);
+		  var today = new Date();
+			this.h = today.getHours();
+			self.m = today.getMinutes();
+			self.s = today.getSeconds();
+			
+			self.m = self.checkTime(self.m);
+			self.s = self.checkTime(self.s);
+	  }, 1000);
+    },
+
+    checkTime: function(i) {
+      if (i < 10) {
+        i = "0" + i;
+	  } // add zero in front of numbers < 10
+	  
+	  console.log("zero please");
+      return i;
+    }
+  },
+  mounted: function() {
+    this.startTime();
+  }
+};
+</script>
+
+<style scoped>
+.content {
+	 position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -50px;
+    margin-left: -50px;
+    width: 100px;
+    height: 100px;
+	font-size:45px;
+}
+</style>
---
+++ clock/resources/js/components/countdown-component.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+          
+          <div class="content">
+            <div id="txt" class="display-1">{{h}}:{{m}}:{{s}}</div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: "countdown-component",
+  data() {
+    return {
+      h: 0,
+      m: 0,
+	  s: 0,
+	  interval: null
+    };
+  },
+  methods: {
+    startTime: function() {
+	  
+	  var self = this;
+	  console.log(this);
+	  // var t = setInterval(this.startTime(), 1000);  // this is relative to setInterval
+      this.interval = setInterval(() => {
+		  console.log(this);
+		  var today = new Date();
+			this.h = today.getHours();
+			self.m = today.getMinutes();
+			self.s = today.getSeconds();
+			
+			self.m = self.checkTime(self.m);
+			self.s = self.checkTime(self.s);
+	  }, 1000);
+    },
+
+    checkTime: function(i) {
+      if (i < 10) {
+        i = "0" + i;
+	  } // add zero in front of numbers < 10
+	  
+	  console.log("zero please");
+      return i;
+    }
+  },
+  mounted: function() {
+    this.startTime();
+  }
+};
+</script>
